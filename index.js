08019237305584522c9f019b211e7df510667d7d
github username <github email address>
2018-08-27 10:02:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file
+function findMatching(array, name){
+  return array.filter(n => n.toUpperCase() === name.toUpperCase())
+}
+
+function fuzzyMatch(array, letters){
+  return array.filter( n=> n.slice(0, letters.length) === letters)
+}
+
+function matchName(array, name){
+  return array.filter(n=> n["name"] === name)
+}
