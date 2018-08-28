9af486407d03b97739caa1a6c1c5fc4dae12a7ce
scarlettperry <si.perry15@gmail.com>
2018-08-28 09:05:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file
+function findMatching(array, name) {
+  return array.filter(word => word.toLowerCase() === name.toLowerCase())
+}
+
+function fuzzyMatch(array, chars) {
+  return array.filter (word => word.startsWith(chars))
+}
+
+function matchName(array, name_match) {
+  return array.filter (user => user.name === name_match)
+}
